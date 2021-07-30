e8d005d24528c2b6397776004896e8844f262051
Wonk6677 <bbament@gmail.com>
2021-07-30 21:48:10 +0100

    0.1
    Still working on Charging setup

---
+++ MG_F_VCU/MG_F_VCU.ino
@@ -0,0 +1,157 @@
+#include <FlexCAN.h>
+//gauges
+int rpm = 5;
+int motortempgauge = 6;
+int fuel = 7;
+
+int dcdcon = 2;
+int dcdccontrol = 8;
+//coolant temp and engine bay fan
+int ThermistorPin = 18;
+int enginefan = 17;
+int Vo;
+int coolanttemp;
+float R1 = 10000;
+float logR2, R2, T;
+float c1 = 0.9818585903e-03, c2 = 1.995199371e-04, c3 = 1.684445298e-07;
+//contactors
+int precharge = 22;
+int maincontactor = 21;
+//Charging
+int cpwm = 24;
+int csdn = 25;
+int accontactor = 30;
+int simpprox = 26;
+int simppilot = 27;
+int chargestart = 28;
+int chargebutton = 31;
+int DCSW = 29;
+
+
+static CAN_message_t rxmsg, txmsg;
+
+
+void setup() {
+
+Can0.begin(500000);
+//outputs
+pinMode(rpm,OUTPUT);
+pinMode(enginefan,OUTPUT);
+pinMode(motortempgauge, OUTPUT);
+pinMode(precharge,OUTPUT);
+pinMode(maincontactor,OUTPUT);
+pinMode(dcdccontrol, OUTPUT);
+pinMode(dcdcon, OUTPUT);
+pinMode(chargestart, OUTPUT);
+pinMode(cpwm, OUTPUT);
+pinMode(csdn, OUTPUT);
+pinMode(accontactor, OUTPUT);
+
+//inputs
+pinMode(simpprox, INPUT_PULLUP);
+pinMode(simppilot, INPUT_PULLUP);
+pinMode(chargebutton, INPUT_PULLUP);
+pinMode(DCSW, INPUT_PULLUP);
+
+delay(3000);
+
+
+//-------If charge port plugged in on startup run through charging setup.
+if (digitalRead(simpprox = LOW)) ///put CPWM and CSDN to High and enable charge mode, disabling drive.
+{
+digitalWrite(cpwm, LOW); 
+digitalWrite(csdn, LOW); 
+
+}
+else // run normal start up
+{
+digitalWrite (precharge, LOW);   //activate prehcharge on start up
+analogWrite(rpm, 128);
+analogWriteFrequency(rpm, 1000); //Start rpm at intial high to simulate engine start.
+delay(3000);
+}
+
+}
+void loop() {
+
+
+
+//----Read canbus messages
+  
+//--------- get 12v voltage
+if (Can0.available()) {
+Can0.read(rxmsg);
+float voltagebig = (( rxmsg.buf[2] << 8) | rxmsg.buf[1]);
+float voltage = voltagebig/32;
+
+///------ get rpm and send to gauge
+
+float rpmraw = (( rxmsg.buf[4] << 8) | rxmsg.buf[3]);
+float rpm = rpmraw/32;
+int rpmpulse = rpm*2;
+if (rpmpulse < 800) //power steering is expecting to see engine idle at least.
+{
+rpmpulse = 802;
+}
+analogWriteFrequency(rpm, rpmpulse); 
+}
+
+//--------contactor close cycle
+// if hv bus is within a few volts of battery voltage and OI is sending close main contactor, close main contactor and open precharge. Also activate dc-dc
+
+
+// when main contactor closes do following
+/*
+analogWriteFrequency(dcdccontrol, 200); //change this number to change dcdc voltage output
+digitalWrite (dcdcon, HIGH);
+*/
+
+//--------Charge process
+digitalRead (simppilot);
+digitalRead (chargebutton);
+digitalRead (DCSW);
+
+if ((simppilot = LOW)&& (chargebutton = LOW))
+{
+digitalWrite (chargestart, LOW); // semd signal to simpcharge to send AC voltage
+delay (5000); //delay to allow precharge
+}
+if ((simppilot = LOW) && (DCSW = LOW) && (chargebutton = LOW)) //needs pilot signal, HV bus precharged and the charge button pressed before charging starts.
+{
+digitalWrite (accontactor, LOW);
+digitalWrite (maincontactor, LOW);
+digitalWrite (csdn, HIGH);
+
+}
+else
+{
+digitalWrite (accontactor, HIGH);
+digitalWrite (chargestart, HIGH);
+//Also send canbus message to inverter to set forward and reverse at same time to enable charge mode
+}
+
+//---------Temperature read
+
+Vo = analogRead(ThermistorPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  T = T - 273.15; //in C
+  coolanttemp = T;
+
+//--------- Activate engine bay fan
+
+if (coolanttemp > 40) 
+{
+digitalWrite(enginefan, LOW); 
+}
+else
+{
+digitalWrite(enginefan, HIGH);
+}
+
+
+
+
+
+}
